e6a55e044b47d01b1c9c0ac381d8900e085a9f54
eldojk <eldojk@gmail.com>
2016-09-14 22:02:17 +0530

    ll done

---
+++ WS/G4G/Problems/stack/stack_with_min.py
@@ -0,0 +1,43 @@
+"""
+Stack with min
+"""
+from sys import maxint
+
+
+class Stack(object):
+    def __init__(self):
+        self.stack = []
+
+    def push(self, data):
+        current_min = self.minimum()
+        tup = (data, current_min if data > current_min else data)
+        self.stack.append(tup)
+        print self.stack
+
+    def pop(self):
+        return self.stack.pop()[0] if len(self.stack) > 0 else None
+
+    def peek(self):
+        return self.stack[-1][1]
+
+    def minimum(self):
+        if len(self.stack) == 0:
+            return maxint
+
+        return self.peek()
+
+# s = Stack()
+# s.push(5)
+# s.push(6)
+# s.push(3)
+# s.push(7)
+#
+# print s.minimum()
+# s.pop()
+# print s.minimum()
+# s.pop()
+# print s.minimum()
+# s.pop()
+# print s.minimum()
+# s.pop()
+# print s.minimum()
