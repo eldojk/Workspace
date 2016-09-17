26ddc849d34f693eec7d962f4ae432508a532b11
eldojk <eldojk@gmail.com>
2016-09-17 12:48:49 +0530

    two amzn ques added

---
+++ WS/G4G/Problems/bst/connect_nodes_at_a_level.py
@@ -0,0 +1,86 @@
+"""
+http://www.geeksforgeeks.org/connect-nodes-at-same-level/
+Write a function to connect all the adjacent nodes at the same level in a binary tree.
+
+Input Tree
+       A
+      / \
+     B   C
+    / \   \
+   D   E   F
+
+
+Output Tree
+       A--->NULL
+      / \
+     B-->C-->NULL
+    / \   \
+   D-->E-->F-->NULL
+"""
+from Queue import Queue
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.left = None
+        self.right = None
+        self.nxt = None
+
+    def __repr__(self):
+        return str(self.data)
+
+
+def bfs_with_levels(root):
+    q = Queue()
+    q2 = Queue()
+    q.put((root, 0))
+    q2.put((root, 0))
+
+    while not q.empty():
+        node_info = q.get()
+        node = node_info[0]
+        level = node_info[1]
+
+        if node.left:
+            q.put((node.left, level + 1))
+            q2.put((node.left, level + 1))
+
+        if node.right:
+            q.put((node.right, level + 1))
+            q2.put((node.right, level + 1))
+
+    return q2
+
+
+def connect_nodes_at_levels(queue):
+    if not queue.empty():
+        left = queue.get()
+
+    while not queue.empty():
+        right = queue.get()
+
+        if left[1] == right[1]:
+            print 'connecting {0} and {1}'.format(left[0], right[0])
+            left[0].nxt = right[0]
+
+        left = right
+
+
+def connect_nodes(root):
+    queue = bfs_with_levels(root)
+    connect_nodes_at_levels(queue)
+
+
+    # if __name__=='__main__':
+    #     root = Node('A')
+    #     root.left = Node('B')
+    #     root.right = Node('C')
+    #     root.left.left = Node('D')
+    #     root.left.right = Node('E')
+    #     root.right.right = Node('F')
+    #
+    #     connect_nodes(root)
+    #     print root.left.nxt
+    #     print root.left.left.nxt
+    #     print root.left.right.nxt
---
+++ WS/G4G/Problems/dp/min_sliding_window_val.py
@@ -0,0 +1,32 @@
+"""
+Given an Array of size n and a sliding window of size k (k << n), If the sliding window slides from the beginning of
+the array, Find the minimum of the values at each position of the window. Find the minimum possible value at all
+positions
+
+[10	20	30	50	10	70	30	50	80	20	10], 3
+[0	0	10	20	10	10	10	30	30	20	10]
+
+	    10	20	30	50	10	70	30	50	80	20	10
+0(1)	10	20	30	50	10	70	30	50	80	20	10
+1(2)	0	10	20	30	10	10	30	30	50	20	10
+2(3)	0	0	10	20	10	10	10	30	30	20	10
+
+minimum in sliding window of size k = MIN (minimum of sliding window of size k-1, array[k])
+"""
+from copy import copy
+
+
+def find_min_sliding_window(array, sw_size):
+    sw_sizes = [[None for i in array] for j in range(sw_size)]
+    sw_sizes[0] = copy(array)
+    print sw_sizes
+
+    for i in range(1, sw_size):
+        for j in range(i, len(array)):
+            print i, j
+            sw_sizes[i][j] = min(sw_sizes[i - 1][j - 1], array[j])
+
+    return sw_sizes[sw_size - 1]
+
+
+#print find_min_sliding_window([10, 20, 30, 50, 10, 70, 30, 50, 80, 20, 10], 3)
