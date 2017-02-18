9e011c7746b894fbd8c5a7828a1be2c526903759
eldojk <eldojk@gmail.com>
2017-02-18 21:24:20 +0530

    removed ood

--- ObjectOrientedDesignJava/src/main/java/ood/objectorienteddesign/callcenter/CallStatus.java
+++
@@ -1,5 +0,0 @@
-package ood.objectorienteddesign.callcenter;
-
-public enum CallStatus {
-	CONNECTING, ATTENDING, RESOLVED, UNRESOLVED, DROPPED
-}
--- ObjectOrientedDesignJava/src/main/java/ood/objectorienteddesign/callcenter/Rank.java
+++
@@ -1,5 +0,0 @@
-package ood.objectorienteddesign.callcenter;
-
-public enum Rank {
-	ATTENDER, MANAGER, DIRECTOR
-}
