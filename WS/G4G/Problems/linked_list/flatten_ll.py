b9d6aa80aa64098ef25b1aaadf9d1ff6ff8d28c2
eldojk <eldojk@gmail.com>
2017-03-02 19:53:20 +0530

    g4g ll problems

---
+++ WS/G4G/Problems/linked_list/flatten_ll.py
@@ -0,0 +1,92 @@
+"""
+http://www.geeksforgeeks.org/flattening-a-linked-list/
+"""
+
+
+class Node(object):
+    def __init__(self, data):
+        self.data = data
+        self.nxt = None
+        self.down = None
+
+    def  __repr__(self):
+        return str(self.data)
+
+
+def merge_list(head1, head2):
+    if head1.data < head2.data:
+        h = head1
+        head1 = head1.down
+    else:
+        h = head2
+        head2 = head2.down
+
+    tail = h
+
+    while head1 is not None and head2 is not None:
+        if head1.data < head2.data:
+            tail.down = head1
+            head1 = head1.down
+            tail = tail.down
+        else:
+            tail.down = head2
+            head2 = head2.down
+            tail = tail.down
+
+    while head1 is not None:
+        tail.down = head1
+        head1 = head1.down
+        tail = tail.down
+
+    while head2 is not None:
+        tail.down = head2
+        head2 = head2.down
+        tail = tail.down
+
+    return h
+
+
+def flatten_list(head):
+    l1 = head
+    l2 = head.nxt
+
+    while l2 is not None:
+        l1 = merge_list(l1, l2)
+        l2 = l2.nxt
+
+    return l1
+
+
+if __name__ == '__main__':
+    l1 = Node(5)
+    l1.down = Node(7)
+    l1.down.down = Node(8)
+    l1.down.down.down = Node(30)
+
+    l2 = Node(10)
+    l2.down = Node(20)
+
+    l1.nxt = l2
+
+    l3 = Node(19)
+    l3.down = Node(22)
+    l3.down.down = Node(50)
+
+    l2.nxt = l3
+
+    l4 = Node(28)
+    l4.down = Node(35)
+    l4.down.down = Node(40)
+    l4.down.down.down = Node(45)
+
+    l3.nxt = l4
+
+
+    h = flatten_list(l1)
+
+    while h.down is not None:
+        print h,
+        h = h.down
+
+
+
---
+++ WS/G4G/Problems/linked_list/flatten_multi_level_ll.py
@@ -0,0 +1,52 @@
+"""
+http://www.geeksforgeeks.org/flatten-a-linked-list-with-next-and-child-pointers/
+"""
+from Queue import Queue
+
+from G4G.Problems.linked_list.flatten_ll import Node
+from G4G.Problems.linked_list.linked_list import create_linked_list
+
+
+def flatten(head):
+    h = head
+    q = Queue()
+
+    while h.nxt is not None:
+        if h.down:
+            q.put(h.down)
+
+        h = h.nxt
+
+    while not q.empty():
+        node = q.get()
+        while node is not None:
+            if node.down:
+                q.put(node.down)
+
+            h.nxt = node
+            node = node.nxt
+            h = h.nxt
+
+    return head
+
+
+if __name__ == '__main__':
+    l1 = create_linked_list([10, 5, 12, 7, 11], Node)
+    l2 = create_linked_list([4, 20, 13], Node)
+    l3 = create_linked_list([17, 6], Node)
+
+    l1.down = l2
+    l1.nxt.nxt.nxt.down = l3
+
+    l2.nxt.down = Node(2)
+    l2.nxt.nxt.down = Node(16)
+    l2.nxt.nxt.down.down = Node(3)
+
+    l3.down = create_linked_list([9, 8], Node)
+    l3.down.down = create_linked_list([19, 15], Node)
+
+    h = flatten(l1)
+
+    while h is not None:
+        print h,
+        h = h.nxt
