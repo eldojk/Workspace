d2cb32124686bc2e0a81a16203f88aab69d3ad05
eldojk <eldojk@gmail.com>
2017-03-08 21:03:47 +0530

    some array problems

--- WS/G4G/Problems/arrays/array_problems.py
+++ WS/G4G/Problems/arrays/array_problems.py
@@ -65,3 +65,152 @@ if __name__ == "__main__":
         "Delhi": "Goa"
     }
     find_itinerary(itinerary)
+
+
+"""
+http://www.geeksforgeeks.org/find-missing-elements-of-a-range/
+"""
+
+
+def print_missing_elements_in_range(array, hi, lo):
+    print ''
+    _range = range(hi, lo + 1)
+    hm = {el: True for el in array}
+
+    for el in _range:
+        if hm.get(el):
+            continue
+        else:
+            print el,
+
+
+if __name__ == "__main__":
+    print_missing_elements_in_range([10, 12, 11, 15], 10, 15)
+    print ''
+
+
+"""
+http://www.geeksforgeeks.org/pair-with-given-product-set-1-find-if-any-pair-exists/
+"""
+
+
+def find_pair_with_given_product(array, product):
+    hm = {}
+    for el in array:
+        if el == 0 and product == 0:
+            return True
+        elif product % el == 0 and hm.get(product/el):
+            return True
+        else:
+            hm[el] = True
+
+    return False
+
+
+if __name__ == '__main__':
+    print ''
+    print find_pair_with_given_product([10, 20, 9, 40], 400)
+    print find_pair_with_given_product([10, 20, 9, 40], 190)
+    print find_pair_with_given_product([-10, 20, 9, -40], 400)
+    print find_pair_with_given_product([-10, 20, 9, 40], -400)
+    print find_pair_with_given_product([0, 20, 9, 40], 0)
+
+
+
+"""
+http://www.geeksforgeeks.org/find-subarray-with-given-sum/
+
+note - this is o(n). check link for proof
+"""
+
+
+def subarray_with_sum(array, sm):
+    curr_sum = array[0]
+    start = 0
+
+    for i in range(1, len(array) + 1):
+
+        while curr_sum > sm and start < i - 1:
+            curr_sum = curr_sum - array[start]
+            start += 1
+
+        if curr_sum == sm:
+            print start, i - 1
+
+        if i < len(array):
+            curr_sum = curr_sum + array[i]
+
+
+if __name__ == '__main__':
+    print ''
+    subarray_with_sum([1, 4, 20, 3, 10, 5], 33)
+    subarray_with_sum([1, 4, 0, 0, 3, 10, 5], 7)
+    subarray_with_sum([1, 4], 0)
+
+
+def sub_array_with_sum_hashmap(array, sm):
+    curr_sum = 0
+    hm = {}
+
+    for i in range(len(array)):
+        el = array[i]
+        curr_sum += el
+
+        if curr_sum == sm:
+            print 0, i
+
+        if hm.get(curr_sum - sm) is not None:
+            indexes = hm[curr_sum - sm]
+            for idx in indexes:
+                print idx + 1, i
+
+        hm[curr_sum] = [i] if hm.get(curr_sum) is None else hm[curr_sum] + [i]
+
+
+if __name__ == '__main__':
+    print ''
+    sub_array_with_sum_hashmap([1, 4, 20, 3, 10, 5], 33)
+    sub_array_with_sum_hashmap([1, 4, 0, 0, 3, 10, 5], 7)
+    sub_array_with_sum_hashmap([1, 4], 0)
+
+
+"""
+http://www.geeksforgeeks.org/print-all-subarrays-with-0-sum/
+"""
+
+
+if __name__ == '__main__':
+    print ''
+    sub_array_with_sum_hashmap([6, 3, -1, -3, 4, -2, 2, 4, 6, -12, -7], 0)
+
+
+"""
+http://www.geeksforgeeks.org/group-shifted-string/
+"""
+
+
+def get_int_rep(string):
+    s = ord(string[0]) - ord('a')
+    rep = ''
+    for i in range(1, len(string)):
+        ascii = ord(string[i]) - ord('a')
+        diff = ascii - s
+        rep += str(diff)
+        s = ascii
+
+    return rep
+
+
+def find_shifted_strings(array):
+    hm = {}
+    for s in array:
+        rep = get_int_rep(s)
+        hm[rep] = [s] if hm.get(rep) is None else hm[rep] + [s]
+
+    for el in hm.keys():
+        print hm[el] if len(hm[el]) > 1 else '',
+
+
+if __name__ == '__main__':
+    print ''
+    find_shifted_strings(["acd", "dfg", "wyz", "yab", "mop", "bdfh", "a", "x", "moqs"])
