425a66ca47423ed3e49c139336674366dd47b56d
Eldo Joseph <eldo.joseph@oracle.com>
2017-03-24 20:59:09 +0530

    dp problems

--- WS/G4G/Problems/dp/longest_palindromic_substring.py
+++ WS/G4G/Problems/dp/longest_palindromic_substring.py
@@ -1,41 +1,57 @@
 """
 http://www.geeksforgeeks.org/longest-palindrome-substring-set-1/
 
-	g	e	k	s	k	g
-g	1	0	0	0	0	0
-e		1	0	0	0	0
-k			1	0	1	0
-s				1	0	0
-k					1	0
-g						1
+        b	a	b	a	c	s
+        0	1	2	3	4	5
+-----------------------------
+b	0	1	1	3	3	3	3
+a	1		1	1	3	3	3
+b	2			1	1	1	1
+a	3				1	1	1
+c	4					1	1
+s	5						1
+
+
+l[i,j] = length of longest palindromic substring from i to j
+
+if i = j :
+    1
+if i - j == 1:
+    2 if a[i] == b[j]
+    1 else
+else:
+    2 + l[i+1, j-1] if a[i] == a[j]
+    max (l[i, j-1], l[i+1,j])  # try string removing first character and last character
 """
+from G4G.Problems.dp.min_matrix_cost_path_to_mn import print_matrix
 
 
 def longest_palindromic_substring(string):
-    matrix = [[False for i in range(len(string))] for j in range(len(string))]
+    dp = [[0 for i in string] for j in string]
 
-    max_len = 1
+    # adding base cases
     for i in range(len(string)):
-        matrix[i][i] = True
+        dp[i][i] = 1
 
-    for i in range(len(string) - 1):
-        if string[i] == string[i + 1]:
-            matrix[i][i + 1] = True
-            max_len = 2
+        j = i + 1
+        if j < len(string):
+            dp[i][j] = 1 if string[i] != string[j] else 2
 
     max_index = len(string) - 1
     for l in range(2, len(string)):
         for i in range(len(string)):
+
             j = i + l
 
             if j <= max_index:
-                matrix[i][j] = (string[i] == string[j]) and matrix[i + 1][j - 1]
+                if string[i] == string[j]:
+                    dp[i][j] = 2 + dp[i + 1][j - 1]
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
 
-                if matrix[i][j]:
-                    length = j - i + 1
-                    if length > max_len:
-                        max_len = length
+    # print_matrix(dp)
+    return dp[0][max_index]
 
-    return max_len
 
-# print longest_palindromic_substring('gekskg')
+if __name__ == '__main__':
+    print longest_palindromic_substring('babacs')
