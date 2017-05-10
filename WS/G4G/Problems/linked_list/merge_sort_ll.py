daf6bc4af6001c197ccf46798d72267e7c4992e8
Eldo Joseph <eldo.joseph@oracle.com>
2017-05-10 19:56:56 +0530

    merge sort ll

---
+++ WS/G4G/Problems/linked_list/merge_sort_ll.py
@@ -0,0 +1,50 @@
+"""
+amzn
+
+http://www.geeksforgeeks.org/merge-sort-for-linked-list/
+"""
+from G4G.Problems.linked_list.linked_list import create_linked_list, print_ll
+from G4G.Problems.linked_list.merge_sorted_ll import sorted_merge
+
+
+def split(head):
+    front = head
+
+    if head is None or head.nxt is None:
+        back = None
+
+    else:
+        f = head.nxt
+        s = head
+
+        while f is not None:
+            f = f.nxt
+
+            if f is not None:
+                s = s.nxt
+                f = f.nxt
+
+        front = head
+        back = s.nxt
+        s.nxt = None
+
+    return front, back
+
+
+def merge_sort_ll(head):
+    if head is None or head.nxt is None:
+        return head
+
+    a, b = split(head)
+
+    a_result = merge_sort_ll(a)
+    b_result = merge_sort_ll(b)
+
+    head = sorted_merge(a_result, b_result)
+    return head
+
+
+if __name__ == '__main__':
+    h = create_linked_list([4, 5, 3, 1, 0, 6, 2])
+    h = merge_sort_ll(h)
+    print_ll(h)
